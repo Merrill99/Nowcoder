6cee7ed5d0ad60873990cabe25f01a5a30480c89
Merrill99 <merrill_0406@qq.com>
2023-08-15 09:48:22 +0800

    优化写法

--- IsPopOrder/IsPopOrder/IsPopOrder.cpp
+++ IsPopOrder/IsPopOrder/IsPopOrder.cpp
@@ -4,32 +4,55 @@
 #include<stack>
 using namespace std;
 
+//class Solution {
+//public:
+//    bool IsPopOrder(vector<int>& pushV, vector<int>& popV) {
+//        stack<int> _st;
+//        int i = 0, j = 0;
+//        while (i < pushV.size())
+//        {
+//            // 先入栈
+//            _st.push(pushV[i]);
+//            ++i;
+//            // 与弹出序列进行比较
+//            // 匹配出栈顶元素，出栈序列++，不匹配入栈
+//            while ((!_st.empty()) && (j < popV.size()) && (_st.top() == popV[j]))
+//            {
+//                _st.pop();
+//                ++j;
+//            }
+//        }
+//        // 如果栈为空，则证明完全匹配
+//        if (_st.empty())
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    }
+//};
+
+// 优化后
 class Solution {
 public:
     bool IsPopOrder(vector<int>& pushV, vector<int>& popV) {
         stack<int> _st;
-        int i = 0, j = 0;
-        while (i < pushV.size())
+        size_t pushi = 0, popi = 0;
+        while (pushi < pushV.size())
         {
             // 先入栈
-            _st.push(pushV[i]);
-            ++i;
+            _st.push(pushV[pushi++]);
             // 与弹出序列进行比较
             // 匹配出栈顶元素，出栈序列++，不匹配入栈
-            while ((!_st.empty()) && (j < popV.size()) && (_st.top() == popV[j]))
+            while ((!_st.empty()) && (_st.top() == popV[popi]))
             {
                 _st.pop();
-                ++j;
+                ++popi;
             }
         }
         // 如果栈为空，则证明完全匹配
-        if (_st.empty())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return _st.empty();
     }
 };
